ac466a1e395ab77850861288ed04564c80ef76d5
Romanio0089 <38210134+Romanio0089@users.noreply.github.com>
2021-08-01 14:17:04 +0200

    Add SNAPSHOT B1 Build

--- src/com/romanio0089/crashessentials/Main.java
+++ src/com/romanio0089/crashessentials/Main.java
@@ -1,7 +1,5 @@
 package com.romanio0089.crashessentials;
 
-import java.util.WeakHashMap;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -9,35 +7,21 @@ import org.bukkit.Particle;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-	
-	String color = "red";
-	WeakHashMap<Location, String> selections = new WeakHashMap<Location, String>();
-
     @Override
     public void onEnable() {
     	
-        getLogger().info("has been sucessfully enabled !");
-        
-        PluginManager pm = getServer().getPluginManager();
-        PlayerJoinMessageListener listener = new PlayerJoinMessageListener(this);
-        pm.registerEvents(listener, this);
-        
-        if ( getConfig().getString("color") == null ) {
-        	getConfig().set("color", "red");
-        	saveConfig();
-        }
-        
+        getLogger().info("Crash Essentials has been sucessfully enabled !");
+    	getServer().getPluginManager().registerEvents(new PlayerJoinMessageListener(null), this);
+        getConfig();
     }
 	
     @Override
     public void onDisable() {
     	
-        getLogger().info("has been sucessfully disabled !");
-        
+        getLogger().info("CrashEssentials has been sucessfully disabled !");    
     }
     
     
@@ -45,35 +29,58 @@ public class Main extends JavaPlugin {
     	
     	Player player = (Player) sender;
     	
-    	if (sender instanceof Player) {
+    	String lowerCmd = cmd.getName().toLowerCase();
     		
-    		String lowerCmd = cmd.getName().toLowerCase();
+    	switch (lowerCmd) {
     		
-    		switch (lowerCmd) {
-    		
+    		case "fakejavaerror":
+    			
+                if (args.length < 2) {
+                    player.sendMessage(ChatColor.GRAY + "[" + ChatColor.RED + "CrashEssentials" + ChatColor.GRAY + "] " + ChatColor.RESET + ChatColor.RED + "Not enough arguments! Usage: /fakejavaerror <Player> <Time>");}
+                
+                if (args.length == 2) {
+                    Player target = Bukkit.getPlayerExact(args[0]);
+                    
+                if (target != null) {
+                   String StringTarget = args[0];
+                   int KickTimer = Integer.parseInt(args[1]);
+                   
+                   player.sendMessage(ChatColor.GRAY + "[" + ChatColor.RED + "CrashEssentials" + ChatColor.GRAY + "] " + ChatColor.RESET + ChatColor.GREEN + StringTarget + " has been kicked off the server for " + KickTimer +" seconds.");
+                   
+          	   	   Bukkit.getScheduler().runTaskTimer(this, new Runnable() {
+              	        int KickTime = KickTimer;
+              	        public void run() {
+                            Player kick = Bukkit.getPlayerExact(args[0]);
+              	           	 	if (this.KickTime == 0) {
+              	              	  	return;}
+                  	            this.KickTime--;
+                  	            if (KickTime != 0) {
+                  	            	if (kick != null) {
+                  	            		kick.kickPlayer("Internal Exception: java.io.IOException: An existing connection was forcibly closed by the remote host");}}}
+          	   	   		}, 0L, 20L);}
+                	return true;}
+                else {
+                    player.sendMessage(ChatColor.GRAY + "[" + ChatColor.RED + "CrashEssentials" + ChatColor.GRAY + "] " + ChatColor.RESET + ChatColor.RED + "Your target '" + args[0] + "' is not online!");}
+            
     		case "realcrash":
-                if (args.length == 0)
-                    player.sendMessage(ChatColor.RED + "Not enough arguments!");
+    			
+                if (args.length == 0) {
+                    player.sendMessage(ChatColor.GRAY + "[" + ChatColor.RED + "CrashEssentials" + ChatColor.GRAY + "] " + ChatColor.RESET + ChatColor.RED + "Not enough arguments!");}
+                
                 if (args.length == 1) {
                     Player target = Bukkit.getPlayerExact(args[0]);
-                if (target == null) {
-                    player.sendMessage(ChatColor.RED + "Your target '" + args[0] + "' is not online!");
-                } else {
-                    String strtarget = args[0];
-                    Location loc = target.getLocation();
-                    player.sendMessage(ChatColor.GREEN + strtarget + " has been sucessfully crashed.");
-                    target.spawnParticle(Particle.MOB_APPEARANCE, loc, 100000000, 0, 0, 0);
-                    target.kickPlayer("Their game crashed!");
-                return true;
-    	}
-    	
-	}
+                    
+                if (target != null) {
+                    String StringTarget = args[0];
+                    Location location = target.getLocation();
+                    player.sendMessage(ChatColor.GRAY + "[" + ChatColor.RED + "CrashEssentials" + ChatColor.GRAY + "] " + ChatColor.RESET + ChatColor.GREEN + StringTarget + " has been sucessfully crashed.");
+                    target.spawnParticle(Particle.MOB_APPEARANCE, location, 2147483647, 0, 0, 0);
+                    target.kickPlayer("Goodbye !");}
+                
+               else {
+                    player.sendMessage(ChatColor.GRAY + "[" + ChatColor.RED + "CrashEssentials" + ChatColor.GRAY + "] " + ChatColor.RESET + ChatColor.RED + "Your target '" + args[0] + "' is not online!");}
+                
+                return true;}}
 		return false;
-    	
 	}
-}
-		return false;
-
-	}
-	
 }
